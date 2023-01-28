9d29633077b011b910a245c80436a0bd19725008
David Moore <dwjmoore@gmail.com>
2023-01-28 13:55:17 +0000

    Finished exercise 88.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		Basic1_87 exercise = new Basic1_87();
+		Basic1_88 exercise = new Basic1_88();
 		exercise.main();
 	}
 }
---
+++ basic-exercises-1/Basic1_88.java
@@ -0,0 +1,10 @@
+public class Basic1_88 {
+	public void main() {
+		System.out.println("Write a Java program to get the current system environment and system properties.");
+
+		System.out.println("\nCurrent system environment:");
+    System.out.println(System.getenv());	 
+		System.out.println("\n\nCurrent system properties:");
+    System.out.println(System.getProperties());
+	}
+}
